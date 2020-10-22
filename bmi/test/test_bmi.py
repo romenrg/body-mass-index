6f2588fd71b85055a3ae94e5e3879e52ae898418
Romén Rodríguez-Gil <romen@romenrg.com>
2020-10-22 16:25:04 +0100

    Creating basic class and including first test

---
+++ bmi/test/test_bmi.py
@@ -0,0 +1,9 @@
+import unittest
+
+from bmi.bmi import Bmi
+
+
+class TestHello(unittest.TestCase):
+    def test_hello(self):
+        test_bmi = Bmi()
+        self.assertEqual(test_bmi.hello(), "This util calculates the Body Mass Index", "Message should match")
