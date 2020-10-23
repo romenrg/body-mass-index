2a659ec81948446911e5476605c02b38770fe29d
Romén Rodríguez-Gil <rrodriguezgil@cloudbees.com>
2020-10-23 16:17:10 +0100

    Added "calculate" as static method (& tests for ir)

--- bmi/bmi.py
+++ bmi/bmi.py
@@ -1,3 +1,9 @@
 class Bmi:
     def hello(self):
         return "This util calculates the Body Mass Index"
+    @staticmethod
+    def calculate(weight_kg, height_m):
+        exact_value = weight_kg / pow(height_m, 2)
+        rounded_value = round(exact_value, 1)
+        print("Exact value: "+ str(exact_value) + "; Rounded value: "+ str(rounded_value))
+        return rounded_value
--- bmi/test/test_bmi.py
+++ bmi/test/test_bmi.py
@@ -5,3 +5,11 @@ class TestHello(unittest.TestCase):
     def test_hello(self):
         test_bmi = Bmi()
         self.assertEqual(test_bmi.hello(), "This util calculates the Body Mass Index", "Message should match")
+    def test_calculate_equal_80_180(self):
+        self.assertEqual(Bmi.calculate(80, 1.80), 24.7, "Body Mass Index for 80kg and 1.80m is 24.7")
+    def test_calculate_equal_64_164(self):
+        self.assertEqual(Bmi.calculate(64, 1.64), 23.8, "Body Mass Index for 64kg and 1.64m is 23.8")
+    def test_calculate_obese_93_171(self):
+        self.assertEqual(Bmi.calculate(93, 1.71), 31.8, "Body Mass Index for 93kg and 1.71m is 31.8")
+    def test_calculate_thin_59_176(self):
+        self.assertEqual(Bmi.calculate(59, 1.76), 19, "Body Mass Index for 59kg and 1.76m is 19")
