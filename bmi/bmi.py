59e002c1d93538ba8a548de1f82679421798da29
Romén Rodríguez-Gil <romen@romenrg.com>
2020-10-23 16:45:47 +0100

    Added "range" as static method (& tests for it)

--- bmi/bmi.py
+++ bmi/bmi.py
@@ -1,9 +1,26 @@
 class Bmi:
     def hello(self):
         return "This util calculates the Body Mass Index"
+
     @staticmethod
     def calculate(weight_kg, height_m):
         exact_value = weight_kg / pow(height_m, 2)
         rounded_value = round(exact_value, 1)
         print("Exact value: "+ str(exact_value) + "; Rounded value: "+ str(rounded_value))
         return rounded_value
+
+    @staticmethod
+    def range(bmi):
+        if bmi < 18.5:
+            return "Underweight"
+        elif (bmi >= 18.5) and (bmi < 24.9):
+            return "Healthy"
+        elif (bmi >= 25) and (bmi < 29.9):
+            return "Overweight"
+        elif (bmi >= 30) and (bmi < 34.9):
+            return "Obese | Class I"
+        elif (bmi >= 35) and (bmi < 39.9):
+            return "Obese | Class II"
+        else:
+            return "Obese | Class III"
+
--- bmi/test/test_bmi.py
+++ bmi/test/test_bmi.py
@@ -5,6 +5,7 @@ class TestHello(unittest.TestCase):
     def test_hello(self):
         test_bmi = Bmi()
         self.assertEqual(test_bmi.hello(), "This util calculates the Body Mass Index", "Message should match")
+
     def test_calculate_equal_80_180(self):
         self.assertEqual(Bmi.calculate(80, 1.80), 24.7, "Body Mass Index for 80kg and 1.80m is 24.7")
     def test_calculate_equal_64_164(self):
@@ -13,3 +14,16 @@ class TestHello(unittest.TestCase):
         self.assertEqual(Bmi.calculate(93, 1.71), 31.8, "Body Mass Index for 93kg and 1.71m is 31.8")
     def test_calculate_thin_59_176(self):
         self.assertEqual(Bmi.calculate(59, 1.76), 19, "Body Mass Index for 59kg and 1.76m is 19")
+
+    def test_range_under(self):
+        self.assertEqual(Bmi.range(17), "Underweight")
+    def test_range_healthy(self):
+        self.assertEqual(Bmi.range(19), "Healthy")
+    def test_range_over(self):
+        self.assertEqual(Bmi.range(25.3), "Overweight")
+    def test_range_obese1(self):
+        self.assertEqual(Bmi.range(30.1), "Obese | Class I")
+    def test_range_obese2(self):
+        self.assertEqual(Bmi.range(36), "Obese | Class II")
+    def test_range_obese3(self):
+        self.assertEqual(Bmi.range(46), "Obese | Class III")
