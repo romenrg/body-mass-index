4168de3750cf2f24145cd9e9a6fdf6905525b047
Romén Rodríguez-Gil <romen@romenrg.com>
2020-12-10 14:38:33 +0000

    Adding "ranges_with_info" & starting firt range in 0 now for simplicity

--- bmi/bmi.py
+++ bmi/bmi.py
@@ -1,11 +1,13 @@
 class Bmi:
     ranges = [
+        0,
         18.5,
         25,
         30,
         35,
         40
     ]
+
     ranges_i18n = {
         0: {
             "en": "Underweight",
@@ -32,6 +34,7 @@ class Bmi:
             "es": "Obesidad | Clase III"
         }
     }
+
     def hello(self):
         return "This util calculates the Body Mass Index"
 
@@ -44,18 +47,29 @@ class Bmi:
 
     @classmethod
     def range(cls, bmi, lang="en"):
-        if bmi < 18.5:
+        if bmi < cls.ranges[1]:
             return cls.ranges_i18n[0][lang]
-        elif (bmi >= cls.ranges[0]) and (bmi < cls.ranges[1]):
-            return cls.ranges_i18n[1][lang]
         elif (bmi >= cls.ranges[1]) and (bmi < cls.ranges[2]):
-            return cls.ranges_i18n[2][lang]
+            return cls.ranges_i18n[1][lang]
         elif (bmi >= cls.ranges[2]) and (bmi < cls.ranges[3]):
-            return cls.ranges_i18n[3][lang]
+            return cls.ranges_i18n[2][lang]
         elif (bmi >= cls.ranges[3]) and (bmi < cls.ranges[4]):
+            return cls.ranges_i18n[3][lang]
+        elif (bmi >= cls.ranges[4]) and (bmi < cls.ranges[5]):
             return cls.ranges_i18n[4][lang]
         else:
             return cls.ranges_i18n[5][lang]
+
+    @classmethod
+    def ranges_with_info(self):
+        detailed_ranges = []
+        for i, range in enumerate(self.ranges):
+            if i + 1 < len(self.ranges):
+                detailed_ranges.append({"From": range, "To": self.ranges[i + 1], "Info": self.range(range)})
+                # print("From: " + str(range) + "; To: "+ str(self.ranges[i + 1]) + "; Info: "+ str(self.range(range)))
+            else:
+                detailed_ranges.append({"From": range, "To": "", "Info": self.range(range)})
+        return detailed_ranges
 
     @classmethod
     def calculate_bmi_with_info(cls, weight_kg, height_m, lang="en"):
@@ -74,11 +88,11 @@ class Bmi:
     @classmethod
     def your_weight_boundaries(cls, height_m):
         weight_per_range = [
-            cls.calculaute_weight(height_m, cls.ranges[0]),
             cls.calculaute_weight(height_m, cls.ranges[1]),
             cls.calculaute_weight(height_m, cls.ranges[2]),
             cls.calculaute_weight(height_m, cls.ranges[3]),
-            cls.calculaute_weight(height_m, cls.ranges[4])
+            cls.calculaute_weight(height_m, cls.ranges[4]),
+            cls.calculaute_weight(height_m, cls.ranges[5])
         ]
         return weight_per_range
 
--- bmi/test/test_bmi.py
+++ bmi/test/test_bmi.py
@@ -6,14 +6,8 @@ class TestHello(unittest.TestCase):
         test_bmi = Bmi()
         self.assertEqual(test_bmi.hello(), "This util calculates the Body Mass Index", "Message should match")
 
-    def test_calculate_equal_80_180(self):
-        self.assertEqual(Bmi.calculate(80, 1.80), 24.7, "Body Mass Index for 80kg and 1.80m is 24.7")
-    def test_calculate_equal_64_164(self):
-        self.assertEqual(Bmi.calculate(64, 1.64), 23.8, "Body Mass Index for 64kg and 1.64m is 23.8")
-    def test_calculate_obese_93_171(self):
-        self.assertEqual(Bmi.calculate(93, 1.71), 31.8, "Body Mass Index for 93kg and 1.71m is 31.8")
-    def test_calculate_thin_59_176(self):
-        self.assertEqual(Bmi.calculate(59, 1.76), 19, "Body Mass Index for 59kg and 1.76m is 19")
+    def test_ranges(self):
+        self.assertEqual(Bmi.ranges, [0, 18.5, 25, 30, 35, 40])
 
     def test_range_under(self):
         self.assertEqual(Bmi.range(17), "Underweight")
@@ -28,6 +22,25 @@ class TestHello(unittest.TestCase):
     def test_range_obese3(self):
         self.assertEqual(Bmi.range(46), "Obese | Class III")
 
+    def test_cranges_with_info(self):
+        self.assertEqual(Bmi.ranges_with_info(), [
+            {'From': 0, 'To': 18.5, 'Info': 'Underweight'},
+            {'From': 18.5, 'To': 25, 'Info': 'Healthy'},
+            {'From': 25, 'To': 30, 'Info': 'Overweight'},
+            {'From': 30, 'To': 35, 'Info': 'Obese | Class I'},
+            {'From': 35, 'To': 40, 'Info': 'Obese | Class II'},
+            {'From': 40, 'To': "", 'Info': 'Obese | Class III'},
+        ])
+
+    def test_calculate_equal_80_180(self):
+        self.assertEqual(Bmi.calculate(80, 1.80), 24.7, "Body Mass Index for 80kg and 1.80m is 24.7")
+    def test_calculate_equal_64_164(self):
+        self.assertEqual(Bmi.calculate(64, 1.64), 23.8, "Body Mass Index for 64kg and 1.64m is 23.8")
+    def test_calculate_obese_93_171(self):
+        self.assertEqual(Bmi.calculate(93, 1.71), 31.8, "Body Mass Index for 93kg and 1.71m is 31.8")
+    def test_calculate_thin_59_176(self):
+        self.assertEqual(Bmi.calculate(59, 1.76), 19, "Body Mass Index for 59kg and 1.76m is 19")
+
     def test_range_under_lang(self):
         self.assertEqual(Bmi.range(17, "es"), "Bajo peso")
 
@@ -38,3 +51,4 @@ class TestHello(unittest.TestCase):
 
     def test_your_weight_boundaries(self):
         self.assertEqual(Bmi.your_weight_boundaries(1.80), [59.9, 81, 97.2, 113.4, 129.6])
+
