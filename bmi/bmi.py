cd82f77e2a1acbf2211d765c0deffb6ff0218ddf
Romén Rodríguez-Gil <rrodriguezgil@cloudbees.com>
2020-12-10 10:33:33 +0000

    Commenting out print for exact bmi value

--- bmi/bmi.py
+++ bmi/bmi.py
@@ -39,7 +39,7 @@ class Bmi:
     def calculate(weight_kg, height_m):
         exact_value = weight_kg / pow(height_m, 2)
         rounded_value = round(exact_value, 1)
-        print("Exact value: "+ str(exact_value) + "; Rounded value: "+ str(rounded_value))
+        # print("Exact value: "+ str(exact_value) + "; Rounded value: "+ str(rounded_value))
         return rounded_value
 
     @classmethod
