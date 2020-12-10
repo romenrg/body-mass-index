6a5a7101d34b08b0885ec46645108013da63798f
Romén Rodríguez-Gil <romen@romenrg.com>
2020-12-10 14:41:47 +0000

    Better naming: range_info method and boundaries array

--- bmi/test/test_bmi.py
+++ bmi/test/test_bmi.py
@@ -7,22 +7,25 @@ class TestHello(unittest.TestCase):
         self.assertEqual(test_bmi.hello(), "This util calculates the Body Mass Index", "Message should match")
 
     def test_ranges(self):
-        self.assertEqual(Bmi.ranges, [0, 18.5, 25, 30, 35, 40])
+        self.assertEqual(Bmi.boundaries, [0, 18.5, 25, 30, 35, 40])
 
-    def test_range_under(self):
-        self.assertEqual(Bmi.range(17), "Underweight")
-    def test_range_healthy(self):
-        self.assertEqual(Bmi.range(19), "Healthy")
-    def test_range_over(self):
-        self.assertEqual(Bmi.range(25.3), "Overweight")
-    def test_range_obese1(self):
-        self.assertEqual(Bmi.range(30.1), "Obese | Class I")
-    def test_range_obese2(self):
-        self.assertEqual(Bmi.range(36), "Obese | Class II")
-    def test_range_obese3(self):
-        self.assertEqual(Bmi.range(46), "Obese | Class III")
+    def test_range_info_under(self):
+        self.assertEqual(Bmi.range_info(17), "Underweight")
+    def test_range_info_healthy(self):
+        self.assertEqual(Bmi.range_info(19), "Healthy")
+    def test_range_info_over(self):
+        self.assertEqual(Bmi.range_info(25.3), "Overweight")
+    def test_range_info_obese1(self):
+        self.assertEqual(Bmi.range_info(30.1), "Obese | Class I")
+    def test_range_info_obese2(self):
+        self.assertEqual(Bmi.range_info(36), "Obese | Class II")
+    def test_range_info_obese3(self):
+        self.assertEqual(Bmi.range_info(46), "Obese | Class III")
 
-    def test_cranges_with_info(self):
+    def test_range_info_under_lang(self):
+        self.assertEqual(Bmi.range_info(17, "es"), "Bajo peso")
+
+    def test_ranges_with_info(self):
         self.assertEqual(Bmi.ranges_with_info(), [
             {'From': 0, 'To': 18.5, 'Info': 'Underweight'},
             {'From': 18.5, 'To': 25, 'Info': 'Healthy'},
@@ -40,9 +43,6 @@ class TestHello(unittest.TestCase):
         self.assertEqual(Bmi.calculate(93, 1.71), 31.8, "Body Mass Index for 93kg and 1.71m is 31.8")
     def test_calculate_thin_59_176(self):
         self.assertEqual(Bmi.calculate(59, 1.76), 19, "Body Mass Index for 59kg and 1.76m is 19")
-
-    def test_range_under_lang(self):
-        self.assertEqual(Bmi.range(17, "es"), "Bajo peso")
 
     def test_calculate_bmi_with_info(self):
         self.assertEqual(Bmi.calculate_bmi_with_info(80, 1.80), (24.7, "Healthy"))
